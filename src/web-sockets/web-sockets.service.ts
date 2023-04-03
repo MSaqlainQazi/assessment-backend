5f1760a36ca4a4131bbf6a72347777ef3fe666d4
saqlain <saqlainm328@gmail.com>
2023-04-03 18:34:36 +0500

    #commit main rm console

--- src/web-sockets/web-sockets.service.ts
+++ src/web-sockets/web-sockets.service.ts
@@ -1,12 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { Server } from 'socket.io';
+import { Injectable } from "@nestjs/common";
+import { Server } from "socket.io";
 
 @Injectable()
 export class WebSocketsService {
   event(server: Server, data: any) {
-    console.log(data);
-    server.emit('emitEvents', {
-      msg: 'get socket events successfully',
+    server.emit("emitEvents", {
+      msg: "get socket events successfully",
       data: data,
     });
   }
